52f5f2be7f9ed9f6146b105222e5e554ca501c18
alansastre <alan@certidevs.com>
2026-04-21 09:59:51 +0200

    Imprimir número de restaurantes en restaurant-list

--- src/main/java/com/restaurantes/controller/RestaurantController.java
+++ src/main/java/com/restaurantes/controller/RestaurantController.java
@@ -1,9 +1,12 @@
 package com.restaurantes.controller;
 
+import com.restaurantes.model.Restaurant;
 import com.restaurantes.repository.RestaurantRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class RestaurantController {
@@ -17,10 +20,13 @@ public class RestaurantController {
 
     // get all restaurants
     // http://localhost:8080/restaurants
-    @GetMapping("restaurants")
+    @GetMapping("restaurants") // controlador
     public String restaurantList(Model model) {
-        model.addAttribute("restaurants",
-                restaurantRepository.findAll());
-        return "restaurants/restaurant-list";
+        // cargar datos en el modelo
+        List<Restaurant> restaurants = restaurantRepository.findAll();
+        model.addAttribute("restaurants", restaurants);
+        model.addAttribute("numRestaurants", restaurants.size());
+        model.addAttribute("title", "Lista de restaurantes");
+        return "restaurants/restaurant-list"; // vista
     }
 }
