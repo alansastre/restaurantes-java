60244a9b2ab61c0e642f7dbf39910f33839eb916
alansastre <alan@certidevs.com>
2026-05-07 11:52:32 +0200

    #4 dish crear nuevo plato dish-form

--- src/main/java/com/restaurantes/controller/DishController.java
+++ src/main/java/com/restaurantes/controller/DishController.java
@@ -10,7 +10,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -58,4 +60,9 @@ public class DishController {
     // GET editDish
 
     // POST saveDish
+    @PostMapping("dishes")
+    public String saveDish(@ModelAttribute Dish dish){
+        dishRepository.save(dish);
+        return "redirect:/dishes";
+    }
 }
