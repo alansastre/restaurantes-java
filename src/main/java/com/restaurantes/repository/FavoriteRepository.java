0e4f130f8ed20fe19fdbf7f550a98233b977de2e
alansastre <alan@certidevs.com>
2026-06-01 12:45:33 +0200

    cambiar botón en función de si restaurant ya es favorito

--- src/main/java/com/restaurantes/repository/FavoriteRepository.java
+++ src/main/java/com/restaurantes/repository/FavoriteRepository.java
@@ -2,9 +2,12 @@ package com.restaurantes.repository;
 
 import com.restaurantes.model.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
 
@@ -14,5 +17,11 @@ public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     Optional<Favorite> findByUser_IdAndRestaurantId(Long userId, Long restaurantId);
     Optional<Favorite> findByUser_IdAndDishId(Long userId, Long dishId);
 
-    // TODO Query para ids
+    // verificar si un restaurante ya es favorito y así decidir mostrarlo diferente en la UI
+    @Query("""
+    select f.restaurant.id from Favorite f
+        where f.user.id = :userId
+        and f.restaurant IS NOT NULL
+    """)
+    Set<Long> findRestaurantIdsByUserId(@Param("userId") Long userId);
 }
