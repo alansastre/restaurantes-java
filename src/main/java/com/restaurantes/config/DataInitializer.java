5a7b9e741960d065d6caf8a6b63244f88c9ba0b0
alansastre <alan@certidevs.com>
2026-06-01 10:43:39 +0200

    mostrar favoritos en user-detail

--- src/main/java/com/restaurantes/config/DataInitializer.java
+++ src/main/java/com/restaurantes/config/DataInitializer.java
@@ -1,9 +1,18 @@
 package com.restaurantes.config;
 
 import com.restaurantes.dto.RegisterForm;
+import com.restaurantes.model.Dish;
+import com.restaurantes.model.Favorite;
+import com.restaurantes.model.Restaurant;
 import com.restaurantes.model.User;
+import com.restaurantes.model.enums.DishType;
+import com.restaurantes.model.enums.FoodType;
 import com.restaurantes.model.enums.Role;
+import com.restaurantes.repository.DishRepository;
+import com.restaurantes.repository.FavoriteRepository;
+import com.restaurantes.repository.RestaurantRepository;
 import com.restaurantes.repository.UserRepository;
+import com.restaurantes.service.FavoriteService;
 import com.restaurantes.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
@@ -18,6 +27,9 @@ Clase para insertar datos demo en el entorno de desarrollo
 @AllArgsConstructor
 public class DataInitializer implements ApplicationRunner {
 
+    private final RestaurantRepository restaurantRepository;
+    private final DishRepository dishRepository;
+    private final FavoriteRepository favoriteRepository;
     private final UserService userService;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -44,5 +56,27 @@ public class DataInitializer implements ApplicationRunner {
                 .imageUrl("/uploads/ollama.png")
                 .build());
 
+
+        // Crear un restaurante  y un plato
+
+        Restaurant restaurant = new Restaurant();
+        restaurant.setName("Restaurant para fav");
+        restaurant.setAveragePrice(20d);
+        restaurant.setNumberEmployees(2);
+        restaurant.setFoodType(FoodType.SPANISH);
+        restaurantRepository.save(restaurant);
+
+        Dish dish = new Dish();
+        dish.setName("Dish para fav");
+        dish.setPrice(10.5d);
+        dish.setDescription("Plato para darle a favoritos");
+        dish.setDishType(DishType.DESSERT);
+        dishRepository.save(dish);
+
+        favoriteRepository.save(Favorite.builder().dish(dish).user(user).build());
+        favoriteRepository.save(Favorite.builder().restaurant(restaurant).user(user).build());
+
+        // guardarlos como favoritos de un usuario
+        // así los podemos mostrar en el user-detail.html
     }
 }
