04bc67c0702230e741d67f8516adaaa03f12b972
alansastre <alan@certidevs.com>
2026-06-08 12:08:48 +0200

    api rest PUT restaurantes editar

--- src/main/java/com/restaurantes/controller/api/RestaurantRestController.java
+++ src/main/java/com/restaurantes/controller/api/RestaurantRestController.java
@@ -43,12 +43,30 @@ public class RestaurantRestController {
 //        return ResponseEntity.created(URI.create("/api/v1/restaurants/" + saved.getId())).body(saved);
 //    }
     @PostMapping
-    public ResponseEntity<Restaurant> save(@RequestBody Restaurant restaurant) {
+    public ResponseEntity<Restaurant> create(@RequestBody Restaurant restaurant) {
         if (restaurant.getId() != null) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Restaurant ID must be null");
         }
         Restaurant saved = restaurantRepository.save(restaurant);
     //        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
         return ResponseEntity.created(URI.create("/api/v1/restaurants/" + saved.getId())).body(saved);
+    }
+
+    // actualizar restaurante
+    @PutMapping("{id}")
+    public ResponseEntity<Restaurant> update(@PathVariable Long id, @RequestBody Restaurant restaurant) {
+        Restaurant existing = restaurantRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Restaurant " + id + " not found")
+        );
+        existing.setName(restaurant.getName());
+        existing.setAveragePrice(restaurant.getAveragePrice());
+        existing.setNumberEmployees(restaurant.getNumberEmployees());
+        existing.setFoodType(restaurant.getFoodType());
+        existing.setImageUrl(restaurant.getImageUrl());
+        existing.setActive(restaurant.getActive());
+        // como alternativa se podría usar DTOs y mappers
+        // existing.setStartDate(restaurant.getStartDate()); // conservar fecha original
+
+        return ResponseEntity.ok(restaurantRepository.save(existing));
     }
 }
