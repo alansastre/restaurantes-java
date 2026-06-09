242ba69baec296cc25e5c9961bc828c318a17060
alansastre <alan@certidevs.com>
2026-06-09 11:36:13 +0200

    opcional documentar api global con bean config oas swagger

---
+++ src/main/java/com/restaurantes/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.restaurantes.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(new Info()
+                .title("API REST de restaurants app made by Alan para la people")
+                .version("v1.0.0")
+                .description("API REST de restaurantes y platos estilo Google Maps pero sin Google ni Maps."));
+    }
+}
